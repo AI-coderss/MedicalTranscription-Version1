712f9709d2069031efe7c255e9f68cd0542a2397
Mohmmed Bahageel <Mohmmed.Bahageel@DSAH.SA>
2024-06-26 16:41:21 +0300

    Initial commit

---
+++ src/Loader.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+
+const Loader = () => (
+<div class="wrapper">
+<div class="loader"></div>
+<div class="wording">
+  <div class="letter">L</div>
+  <div class="letter">o</div>
+  <div class="letter">a</div>
+  <div class="letter">d</div>
+  <div class="letter">i</div>
+  <div class="letter">n</div>
+  <div class="letter">g</div>
+  <div class="letter circle"></div>
+  <div class="letter circle"></div>
+  <div class="letter circle"></div>
+</div>
+</div>
+);
+
+export default Loader;
